411fba1d9ac0cd97271743bae2e596ad325598cf
amithreddy258 <20bds006@iiitdwd.ac.in>
2022-03-16 15:17:29 +0530

    Minor upd

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/page/course_page.dart';
 import 'package:flutter_application_1/page/login_page.dart';
 import 'package:flutter_application_1/quiz/quiz_page.dart';
+import 'package:flutter_application_1/widget/themes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'page/home_page.dart';
@@ -13,11 +14,9 @@ void main() {
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      themeMode: ThemeMode.light,
-      darkTheme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        brightness: Brightness.dark,
-      ),
+      themeMode: ThemeMode.system,
+      theme: MyTheme.lighttheme(context),
+      darkTheme: MyTheme.darktheme(context),
       initialRoute: '/',
       routes: {
         '/': (context) => LoginPage(),
